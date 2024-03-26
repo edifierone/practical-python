09b9134be185045d27aa09a4b7815e26e6130603
Fang (HWTE) Zheng <fang_zheng@apple.com>
2024-03-26 14:34:40 +0800

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -9,17 +9,27 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month = 1
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
-while principal > 0 : 
-  while month <= 12 :
-    principal = principal * (1+rate/12) - (payment +1000)
-    total_paid = total_paid + (payment +1000)
+while principal > 0 and principal >= payment:
+  if month <= 12 :
+    principal = principal * (1+rate/12) - (payment +extra_payment)
+    total_paid = total_paid + (payment +extra_payment)
     month = month + 1
+    print (month , total_paid, principal)
+  elif month > extra_payment_start_month and month <= extra_payment_end_month:
+    principal = principal * (1+rate/12) - (payment + extra_payment)
+    total_paid = total_paid + payment + extra_payment
+    month = month + 1
+    print (month , total_paid, principal)
   else:
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
     month = month + 1
-    
-    
+    print (month , total_paid, principal)
+
+
 print('Total paid', total_paid)
 print ('The number of months', month)
